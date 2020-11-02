4919c9c333c520c3c313557ebe1638299471e10e
stjohn <52610738+sequent5@users.noreply.github.com>
2020-11-02 10:39:45 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,2 @@
-nohup java -jar -Xms40m -Xmx40m -XX:MaxDirectMemorySize=10M -XX:MaxMetaspaceSize=80m /opt/jar/admin.jar --spring.config.location=/opt/config/admin.yaml &
+nohup java -jar -Xms40m -Xmx40m -XX:MaxDirectMemorySize=10M -XX:MaxMetaspaceSize=80m /opt/jar/admin.jar --spring.config.location=/opt/jar/config/admin.yaml &
 nginx -g 'daemon off;'
