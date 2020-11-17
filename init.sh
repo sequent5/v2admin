7ef3a0c6bbe144f609e850d6fe166cce150146bd
stjohn <52610738+sequent5@users.noreply.github.com>
2020-11-17 12:23:21 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
-cp  /opt/jar/config/v2admin.conf /etc/nginx/conf.d/default.conf
+cp  /opt/jar/config/web.conf /etc/nginx/conf.d/default.conf
 nohup java -jar -Xms40m -Xmx40m -XX:MaxDirectMemorySize=10M -XX:MaxMetaspaceSize=80m /opt/jar/admin.jar --spring.config.location=/opt/jar/config/admin.yaml &
 nginx -g 'daemon off;'
